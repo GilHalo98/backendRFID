c12f96a8ca94630a2bdc2c847401b9e59b17455e
halo98 <halo98@pop-os.localdomain>
2024-09-02 10:50:58 -0600

    cambios en controlador de modificar datos de usuario

--- api/controllers/controladorUsuarios.js
+++ api/controllers/controladorUsuarios.js
@@ -203,7 +203,7 @@ exports.registrarUsuario = async(request, respuesta) => {
 };
 
 // Modifica un registro de la base de datos.
-exports.modificarUsuario = async(request, respuesta) => {
+exports.OLDmodificarUsuario = async(request, respuesta) => {
     // PUT Request.
     const cabecera = request.headers;
     const cuerpo = request.body;
@@ -317,6 +317,105 @@ exports.modificarUsuario = async(request, respuesta) => {
         });
     }
 };
+
+// Modifica un registro de la base de datos.
+exports.modificarUsuario = async(request, respuesta) => {
+    // PUT Request.
+    const cabecera = request.headers;
+    const cuerpo = request.body;
+    const parametros = request.params;
+    const consulta = request.query;
+
+    try {
+        // Desencriptamos el payload del token.
+        const payload = await getTokenPayload(cabecera.authorization);
+
+        // Verificamos que el payload sea valido.
+        if(!payload) {
+            return respuesta.status(200).send({
+                codigoRespuesta: CODIGOS.TOKEN_INVALIDO
+            });
+        }
+
+        // Instanciamos la fecha de la modificacion del registro.
+        const fecha = new Date();
+
+        // Recuperamos la informacion del registro.
+        const id = consulta.id;
+        const nombreUsuario = cuerpo.nombreUsuario;
+        const password = cuerpo.password;
+        const idRegistroEmpleadoVinculado = cuerpo.idRegistroEmpleadoVinculado;
+
+        // Verificamos que exista un id del registro a modificar.
+        if(!id) {
+            // Si no, retornamos un mensaje de error.
+            return respuesta.status(200).send({
+                codigoRespuesta: CODIGOS.DATOS_BUSQUEDA_INCOMPLETOS
+            });
+        }
+
+        // Buscamos el registro.
+        const registro = await Usuarios.findByPk(id);
+
+        // Verificamos que exista el registro.
+        if(!registro) {
+            // Si no se encontro el registro, se envia un
+            // codio de registro inexistente.
+            return respuesta.status(200).send({
+                codigoRespuesta: CODIGOS.ZONA_NO_ENCONTRADO,
+            });
+        }
+
+        // Cambiamos los datos del registro.
+        if(nombreUsuario) {
+            registro.nombreUsuario = nombreUsuario;
+        }
+        if(password) {
+            // Si es la misma, se cambia a la nueva contraseña.
+            registro.password = password;
+
+            } else {
+                // Si no se pasa la contraseña anterior
+                // se manda un mensaje
+                return respuesta.status(200).send({
+                    codigoRespuesta: CODIGOS.DATOS_PARA_MODIFICACION_INCOMPLETOS
+                })
+            }
+        }
+        if(idRegistroEmpleadoVinculado) {
+            // Buscamos el registro vinculado.
+            if(! await existeRegistro(Empleados, consulta.idRegistroEmpleadoVinculado)) {
+                return respuesta.status(200).send({
+                    codigoRespuesta: CODIGOS.REGISTRO_VINCULADO_NO_EXISTE
+                });
+            }
+
+            // Si existe el registro, se realiza el cambio.
+            registro.idRegistroEmpleadoVinculado = idRegistroEmpleadoVinculado;
+        }
+
+        // Actualizamos la fehca de modificacion del registro.
+        registro.fechaModificacionUsuario = fecha;
+
+        // Guardamos los cambios.
+        await registro.save();
+
+        // Retornamos un mensaje de operacion ok.
+        return respuesta.status(200).send({
+            codigoRespuesta: CODIGOS.OK
+        });
+
+    } catch(excepcion) {
+        // Mostramos el error en la consola
+        mostrarLog(`Error con controlador: ${excepcion}`);
+
+        // Retornamos un codigo de error.
+        return respuesta.status(500).send({
+            codigoRespuesta: CODIGOS.API_ERROR,
+        });
+    }
+};
+
 
 // Elimina un registro de la base de datos dado un id.
 exports.eliminarUsuario = async(request, respuesta) => {
