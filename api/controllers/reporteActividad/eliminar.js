dec1e15ba436f80b1cb6205c111bfe3c45718de8
halo98 <halo98@pop-os.localdomain>
2024-09-12 15:37:55 -0600

    nueva estructura de controladores agregada

---
+++ api/controllers/reporteActividad/eliminar.js
@@ -0,0 +1,86 @@
+// Modelos de la DB
+const db = require("../../models/index");
+
+// Codigos de la API.
+const respuestas = require("../../utils/codigosAPI");
+
+// Funciones del token
+const {
+    getTokenPayload
+} = require('../../utils/jwtConfig')
+
+// Instanciamos los codigos.
+const CODIGOS = new respuestas.CodigoApp();
+
+// Funciones extra.
+const {
+    mostrarLog
+} = require("../../utils/logs");
+
+// Modelos que usara el controlador.
+const ReportesActividades = db.reporteActividad;
+
+// Elimina un registro de la base de datos dado un id.
+module.exports = async function eliminarReporteActividad(
+    request,
+    respuesta
+) {
+    // DELETE Request.
+    const cabecera = request.headers;
+    const cuerpo = request.body;
+    const parametros = request.params;
+    const consulta = request.query;
+
+    try {
+        // Desencriptamos el payload del token.
+        const payload = await getTokenPayload(cabecera.authorization);
+
+        // Verificamos que el payload sea valido.
+        if(!payload) {
+            return respuesta.status(200).send({
+                codigoRespuesta: CODIGOS.TOKEN_INVALIDO
+            });
+        }
+
+        // Recuperamos los parametros del request.
+        const id = consulta.id;
+
+        // Verificamos que los datos para la operacion esten completos.
+        if(!id) {
+            // Si no, retornamos un mensaje de error.
+            return respuesta.status(200).send({
+                codigoRespuesta: CODIGOS.DATOS_BUSQUEDA_INCOMPLETOS
+            });
+        }
+
+        // Busca el registro dado el id.
+        const registro = await ReportesActividades.findByPk(id);
+
+        // Si no existe el registro con el id.
+        if(!registro) {
+            // Retorna un error.
+            return respuesta.status(200).send({
+                codigoRespuesta: CODIGOS.REPORTE_NO_ENCONTRADO,
+            });
+        }
+
+        // Eliminamos el registro.
+        await registro.destroy();
+
+        // Retornamos la respuesta de operacion ok
+        // y el registro eliminado.
+        return respuesta.status(200).send({
+            codigoRespuesta: CODIGOS.OK,
+            registroEliminado: registro
+        })
+
+    } catch(excepcion) {
+        // Mostramos el error en la consola
+        mostrarLog(`Error con controlador: ${excepcion}`);
+
+        // Retornamos un codigo de error.
+        return respuesta.status(500).send({
+            codigoRespuesta: CODIGOS.API_ERROR,
+        });
+    }
+};
