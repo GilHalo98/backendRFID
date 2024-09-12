f8b59d80905b10affba56424016941b063c35b4b
halo98 <halo98@pop-os.localdomain>
2024-09-12 15:48:27 -0600

    nueva estructura de controladores agregada

---
+++ api/controllers/reporteDispositivo/consultar.js
@@ -0,0 +1,127 @@
+// Modelos de la DB
+const db = require("../../models/index");
+
+// Codigos de la API.
+const respuestas = require("../../utils/codigosAPI");
+
+// Funciones del token
+const {
+    getTokenPayload
+} = require('../../utils/jwtConfig')
+
+// Instanciamos los codigos.
+const CODIGOS = new respuestas.CodigoApp();
+
+// Funcion para verificar que el registro exista en la DB.
+const {
+    existeRegistro
+} = require("../../utils/registros");
+
+// Funciones extra.
+const {
+    mostrarLog
+} = require("../../utils/logs");
+
+// Modelos que usara el controlador.
+const Reportes = db.reporte;
+const Dispositivos = db.dispositivoIoT;
+const ReportesDispositivos = db.reporteDispositivo;
+
+// Consulta los registros en la base de datos.
+module.exports = async function consultaReporteDispositivo(
+    request,
+    respuesta
+) {
+    // GET Request.
+    const cabecera = request.headers;
+    const cuerpo = request.body;
+    const parametros = request.params;
+    const consulta = request.query;
+
+    try {
+        // Desencriptamos el payload del token.
+        const payload = await getTokenPayload(cabecera.authorization);
+
+        // Verificamos que el payload sea valido.
+        if(!payload) {
+            return respuesta.status(200).send({
+                codigoRespuesta: CODIGOS.TOKEN_INVALIDO
+            });
+        }
+
+        // Verificamos si se selecciono un offset.
+        const offset = (
+            !consulta.offset ? consulta.offset : parseInt(consulta.offset)
+        );
+
+        // Verificamos si se selecciono un maximo de elementos por pagina.
+        const limit = (
+            !consulta.limit ? consulta.limit : parseInt(consulta.limit)
+        );
+
+        // Construimos la consulta hacia la db.
+        const datos = Object();
+
+        // Agregamos los parametros de la consulta.
+        if(consulta.id) {
+            datos.id = consulta.id;
+        }
+
+        if(consulta.idReporteVinculado) {
+            // Si no existe.
+            if(! await existeRegistro(Reportes, consulta.idReporteVinculado)) {
+                // Retornamos un mensaje de error.
+                return respuesta.status(200).send({
+                    codigoRespuesta: CODIGOS.REGISTRO_VINCULADO_NO_EXISTE
+                });
+            }
+
+            // Si existe, se agrega el dato a la busqueda.
+            datos.idReporteVinculado = consulta.idReporteVinculado;
+        }
+
+        if(consulta.idDispositivoVinculado) {
+            // Si no existe.
+            if(! await existeRegistro(
+                Dispositivos,
+                consulta.idDispositivoVinculado
+            )) {
+                // Retornamos un mensaje de error.
+                return respuesta.status(200).send({
+                    codigoRespuesta: CODIGOS.REGISTRO_VINCULADO_NO_EXISTE
+                });
+            }
+
+            // Si existe, se agrega el dato a la busqueda.
+            datos.idDispositivoVinculado = consulta.idDispositivoVinculado;
+        }
+
+        // Consultamos el total de los registros.
+        const totalRegistros = await ReportesDispositivos.count({
+            where: datos,
+        });
+
+        // Consultamos todos los registros.
+        const registros = await ReportesDispositivos.findAll({
+            offset: offset,
+            limit: limit,
+            where: datos
+        });
+
+        // Retornamos los registros encontrados.
+        return respuesta.status(200).send({
+            codigoRespuesta: CODIGOS.OK,
+            totalRegistros: totalRegistros,
+            registros: registros
+        });
+
+    } catch(excepcion) {
+        // Mostramos el error en la consola
+        mostrarLog(`Error con controlador: ${excepcion}`);
+
+        // Retornamos un codigo de error.
+        return respuesta.status(500).send({
+            codigoRespuesta: CODIGOS.API_ERROR,
+        });
+    }
+};
