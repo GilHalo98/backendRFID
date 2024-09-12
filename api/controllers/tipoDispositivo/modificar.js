19a35baef8549f6fb0c07a52450367cb8933528b
halo98 <halo98@pop-os.localdomain>
2024-09-12 15:59:35 -0600

    nueva estructura de controladores agregada

---
+++ api/controllers/tipoDispositivo/modificar.js
@@ -0,0 +1,101 @@
+// Modelos de la DB
+const db = require("../../models/index");
+
+// Codigos de la API.
+const respuestas = require("../../utils/codigosAPI");
+
+// Instanciamos los codigos.
+const CODIGOS = new respuestas.CodigoApp();
+
+// Para la creacion y lectura de tokens.
+const {
+    getTokenPayload
+} = require("../../utils/jwtConfig");
+
+// Funciones extra.
+const {
+    mostrarLog
+} = require("../../utils/logs");
+
+// Modelos que usara el controlador.
+const TiposDispositivos = db.tipoDispositivo;
+
+// Modifica un registro de la base de datos.
+module.exports = async function modificarTipoDispositivo(
+    request,
+    respuesta
+) {
+    // PUT Request.
+    const cabecera = request.headers;
+    const cuerpo = request.body;
+    const parametros = request.params;
+    const consulta = request.query;
+
+    try {
+        // Desencriptamos el payload del token.
+        const payload = await getTokenPayload(cabecera.authorization);
+
+        // Verificamos que el payload sea valido.
+        if(!payload) {
+            return respuesta.status(200).send({
+                codigoRespuesta: CODIGOS.TOKEN_INVALIDO
+            });
+        }
+
+        // Instanciamos la fecha de la modificacion del registro.
+        const fecha = new Date();
+
+        // Recuperamos la informacion del registro.
+        const id = consulta.id;
+        const nombreTipoDispositivo = cuerpo.nombreTipoDispositivo;
+        const descripcionTipoDispositivo = cuerpo.descripcionTipoDispositivo;
+
+        // Verificamos que exista un id del registro a modificar.
+        if(!id) {
+            // Si no, retornamos un mensaje de error.
+            return respuesta.status(200).send({
+                codigoRespuesta: CODIGOS.DATOS_BUSQUEDA_INCOMPLETOS
+            });
+        }
+
+        // Buscamos el registro.
+        const registro = await TiposDispositivos.findByPk(id);
+
+        // Verificamos que exista el registro.
+        if(!registro) {
+            // Si no se encontro el registro, se envia un
+            // codio de registro inexistente.
+            return respuesta.status(200).send({
+                codigoRespuesta: CODIGOS.TIPO_DISPOSITIVO_NO_ENCONTRADO,
+            });
+        }
+
+        // Cambiamos los datos del registro.
+        if(nombreTipoDispositivo) {
+            registro.nombreTipoDispositivo = nombreTipoDispositivo;
+        }
+        if(descripcionTipoDispositivo) {
+            registro.descripcionTipoDispositivo = descripcionTipoDispositivo;
+        }
+
+        // Actualizamos la fehca de modificacion del registro.
+        registro.fechaModificacionTipoDispositivo = fecha;
+
+        // Guardamos los cambios.
+        await registro.save();
+
+        // Retornamos un mensaje de operacion ok.
+        return respuesta.status(200).send({
+            codigoRespuesta: CODIGOS.OK
+        });
+
+    } catch(excepcion) {
+        // Mostramos el error en la consola
+        mostrarLog(`Error con controlador: ${excepcion}`);
+
+        // Retornamos un codigo de error.
+        return respuesta.status(500).send({
+            codigoRespuesta: CODIGOS.API_ERROR,
+        });
+    }
+};
