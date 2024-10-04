a5489fe26ad1b05b2f922c99355ecdb8ee19b51f
halo98 <halo98@pop-os.localdomain>
2024-10-04 12:55:44 -0600

    endpoint para el navegador del reporte a detalle de las horas trabajadas terminado

--- api/controllers/reportes/diasReportes.js
+++ api/controllers/reportes/diasReportes.js
@@ -9,7 +9,7 @@ const CODIGOS = new respuestas.CodigoApp();
 
 // Operadores de sequelize para consultas
 const {
-    Op
+    Op, DataTypes
 } = require("sequelize");
 
 // Para la creacion y lectura de tokens.
@@ -22,11 +22,133 @@ const {
     mostrarLog
 } = require("../../utils/logs");
 
+// Funciones de manipulacion del tiempo.
+const {
+    rangoDia,
+    dateDiaSemana,
+    deserealizarSemana,
+} = require("../../utils/tiempo");
+
 // Modelos que usara el controlador.
+const Reportes = db.reporte;
 const Horarios = db.horario;
 const Empleados = db.empleado;
 const DiasLaborales = db.diaLaboral;
+const TiposReportes = db.tipoReporte;
+const ReportesChequeos = db.reporteChequeo;
 
+// Formateamos los datos de los registros.
+async function formatearRegistros(
+    registroEmpleado,
+    diasLaborales,
+    semanaReporte,
+    hoy,
+    tipoReporteEntrada,
+    tipoReporteEntradaRetraso,
+    tipoReporteSalida,
+    tipoReporteSalidaExtras
+) {
+    // Lista de datos formateados.
+    const datosFormateados = [];
+
+    // Por cada registro de los dias laborales.
+    for(let i = 0; i < diasLaborales.length; i ++) {
+        // Desempaquetamos un registro de los dias laborales.
+        const diaLaboral = diasLaborales[i];
+
+        // Instanciamos la fecha del dia.
+        const rangoDiaReporte = rangoDia(
+            diaLaboral.dia,
+            semanaReporte
+        );
+
+        // Instanciamos el dia de la semana.
+        const diaSemana = dateDiaSemana(diaLaboral.dia, semanaReporte, true);
+
+        // Consultamos el reporte de entrada.
+        const reporteEntrada = await ReportesChequeos.findOne({
+            where: {
+                idEmpleadoVinculado: registroEmpleado.id,
+                fechaRegistroReporteChequeo: {
+                    [Op.between]: rangoDiaReporte
+                }
+            },
+            include: [{
+                required: true,
+                model: Reportes,
+                where: {
+                    idTipoReporteVinculado: {
+                        [Op.or]: [
+                            tipoReporteEntrada.id,
+                            tipoReporteEntradaRetraso.id
+                        ]
+                    }
+                },
+                include: [{
+                    model: TiposReportes
+                }]
+            }]
+        });
+
+        // Consultamos el reporte de salida.
+        const reporteSalida = await ReportesChequeos.findOne({
+            where: {
+                idEmpleadoVinculado: registroEmpleado.id,
+                fechaRegistroReporteChequeo: {
+                    [Op.between]: rangoDiaReporte
+                }
+            },
+            include: [{
+                required: true,
+                model: Reportes,
+                where: {
+                    idTipoReporteVinculado: {
+                        [Op.or]: [
+                            tipoReporteSalida.id,
+                            tipoReporteSalidaExtras.id
+                        ]
+                    }
+                },
+                include: [{
+                    model: TiposReportes
+                }]
+            }]
+        });
+
+        // Inicializamos los datos del reporte.
+        let falto = false;
+        let descansoLaborado = false;
+
+        // Si el dia es descanso.
+        if(diaLaboral.esDescanso) {
+            // Si hay registros de entrada y salida
+            if(reporteEntrada && reporteSalida) {
+                // entonces se toma como un descanso laborado.
+                descansosLaborados = true;
+            }
+
+        // Si el dia es uno normal.
+        } else {
+            // Si no hay registro de entrada y salida.
+            if(!reporteEntrada && !reporteSalida) {
+                // Se marca como falta.
+                falto = true;
+            }
+        }
+
+        // Guardamos el dato formateado en la lista.
+        datosFormateados.push({
+            dia: diaLaboral.dia,
+            esDescanso: diaLaboral.esDescanso,
+            falto: falto,
+            diaFueraDeRango: diaSemana > hoy,
+            descansoLaborado: descansoLaborado
+        });
+    }
+
+    // Retornamos los datos formateados.
+    return datosFormateados;
+};
 
 // Lista los dias del horario del empleado junto con una
 // serie de banderas por dia.
@@ -50,6 +172,11 @@ module.exports = async function listarDiasHorario(request, respuesta) {
 
         // Instanciamos la fecha del dia actual.
         const hoy = new Date();
+
+        // Instanciamos la semana del reporte.
+        const semanaReporte = deserealizarSemana(
+            consulta.semanaReporte
+        );
 
         // Desempaquetamos los datos.
         const idEmpleado = consulta.idEmpleado;
@@ -103,13 +230,57 @@ module.exports = async function listarDiasHorario(request, respuesta) {
             });
         }
 
-        // Procesamos los registros de los dias laborales.
-        const listaDias = diasLaborales.map((registro) => {
-            return {
-                dia: registro.dia,
-                esDescanso: registro.esDescanso
-            };
+        // Buscamos el tipo de reporte para entrada.
+        const tipoReporteEntrada = await TiposReportes.findOne({
+            where: {
+                tagTipoReporte: 'chequeoEntrada'
+            }
         });
+
+        // Buscamos el tipo de reporte para entrada con retraso.
+        const tipoReporteEntradaRetraso = await TiposReportes.findOne({
+            where: {
+                tagTipoReporte: 'chequeoEntradaRetraso'
+            }
+        });
+
+        // Buscamos el tipo de reporte para salida.
+        const tipoReporteSalida = await TiposReportes.findOne({
+            where: {
+                tagTipoReporte: 'chequeoSalida'
+            }
+        });
+
+        // Buscamos el tipo de reporte para salida con horas extra.
+        const tipoReporteSalidaExtras = await TiposReportes.findOne({
+            where: {
+                tagTipoReporte: 'chequeoSalidaExtras'
+            }
+        });
+
+        // Si alguno de los registros no existe.
+        if(!tipoReporteEntrada
+            || !tipoReporteEntradaRetraso
+            || !tipoReporteSalida
+            || !tipoReporteSalidaExtras
+        ) {
+            // Retornamos un mensaje de error.
+            return respuesta.status(200).send({
+                codigoRespuesta: CODIGOS.REGISTRO_VINCULADO_NO_EXISTE
+            });
+        }
+
+        // Procesamos los registros de los dias laborales.
+        const listaDias = await formatearRegistros(
+            registroEmpleado,
+            diasLaborales,
+            semanaReporte,
+            hoy,
+            tipoReporteEntrada,
+            tipoReporteEntradaRetraso,
+            tipoReporteSalida,
+            tipoReporteSalidaExtras
+        );
 
         // Retornamos los registros encontrados.
         return respuesta.status(200).send({
