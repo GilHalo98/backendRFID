86b4f953226e0adaf366e3324b25d2289fc2e0f8
halo98 <halo98@pop-os.localdomain>
2024-10-09 11:15:50 -0600

    se agrego la carga dinamica y estatica de controladores

--- api/controllers/tipoDispositivo/index.js
+++ api/controllers/tipoDispositivo/index.js
@@ -1,39 +1,32 @@
-// Libreria estandar para la manipulacion de directorios.
+// Funcion de carga dinamica de los controladores.
 const {
-    parse,
-    join
-} = require('path');
+    cargaDinamicaControladores
+} = require('../../utils/controladores.js');
 
-// Libreria estandar para la manipulacion de archivos.
-const {
-    readdirSync
-} = require('fs');
+function cargaEstaticaControladores() {
+    // Controladores a exportar.
+    const controladores = {};
 
-// Controladores a exportar.
-const controladores = {};
+    // Importamos los controladores.
+    const consultaTipoDispositivo = require('./consultar.js');
+    const eliminarTipoDispositivo = require('./eliminar.js');
+    const modificarTipoDispositivo = require('./modificar.js');
+    const registrarTipoDispositivo = require('./registrar.js');
 
-// Resolvemos el directorio de los controladores.
-const directorioResuelto = __dirname;
+    // Instanciamos una lista de las funciones de los controladores.
+    const funciones = [
+        consultaTipoDispositivo,
+        eliminarTipoDispositivo,
+        modificarTipoDispositivo,
+        registrarTipoDispositivo
+    ];
 
-// Consultamos todos los archivos en el directorio.
-const archivos = readdirSync(directorioResuelto);
+    return controladores;
+};
 
-// Por cada archivo en le directorio.
-archivos.forEach(archivo => {
-    // Instanciamos el objeto archivo.
-    const objetoArchivo = parse(archivo);
-
-    // Si el archivo no es index y es un archivo javascript.
-    if(objetoArchivo.name != 'index' && objetoArchivo.ext == '.js') {
-        // Importamos el controlador.
-        const controlador = require(
-            join(directorioResuelto, objetoArchivo.name)
-        );
-
-        // Agregamos el controlador a la lista de controladores.
-        controladores[controlador.name] = controlador;
-    }
-});
+// Funciones de los controladores.
+// const controladores = cargaDinamicaControladores(__dirname);
+const controladores = cargaEstaticaControladores();
 
 // Exportamos los controladores.
 module.exports = controladores;
